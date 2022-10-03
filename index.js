d7456c44d66958f322133429c5b4995f69cec0ee
tyler ferre <ferre.tyler1@gmail.com>
2022-10-02 21:51:55 -0600

    clean up. Edit button removes but not change text

--- index.js
+++ index.js
@@ -1,91 +1,3 @@
-// var form = document.inputForm
-// var list = document.getElementById("list")
-
-// form.addEventListener("submit", function(e){
-//     e.preventDefault()
-//   var listItem = form.item.value
-
-//   form.item.value = ""
-  
-//   //create list and append it to list
-//     var newItem = document.createElement("li")
-//     // newItem.classList.add("remove")
-//     newItem.textContent = listItem
-//     list.append(newItem)
-  
-//     //create buttons
-  
-//     //delete
-//     var removeButton = document.createElement("button")
-//     removeButton.textContent = "X"
-//     removeButton.classList.add("remove");
-  
-//     //mark
-//     var markButton = document.createElement("button")
-//     markButton.classList.add("mark")
-//     markButton.textContent = "Mark"
-    
-//     //create div for list items
-//     //prepend div to created list
-//     var newDiv = document.createElement("div")
-//     newItem.prepend(newDiv)
-    
-//   //append edit and delete button to created list
-//   newItem.append(markButton, removeButton)
-    
-//         newItem.style.margin = "auto"
-//         newItem.style.fontFamily = "tahoma"
-//         removeButton.style.display = "flex"
-//         removeButton.style.margin = "auto"
-//         markButton.style.display = "flex"
-//         markButton.style.margin = "auto"
-  
-//   //button functionality
-//     removeButton.addEventListener("click", deleteItem)
-//     function deleteItem(event){
-//         if(event.target.classList.contains("remove")){
-//             event.target.closest("li").remove();
-//         }
-//     }
-
-//     //mark button
-//     markButton.addEventListener("click", markItem)
-//     function markItem(event){
-//         if(event.target.classList.contains("mark")){
-//             event.target.closest("li")
-//             newItem.style.textDecoration = "line-through"
-//             newItem.style.textDecorationColor = "crimson"
-//             newItem.style.textDecorationThickness = "2px"
-//         }
-//     }
-
-
-// })
-
-// document
-// .getElementById("enterButton")
-// .addEventListener("mouseover", () => {
-//     enterButton.style.color = "green"
-// })
-
-// document
-// .getElementById("enterButton")
-// .addEventListener("mouseleave", () => {
-//     enterButton.style.color = "black"
-// })
-
-// document
-// .getElementById("enterButton")
-// .addEventListener("mousedown", () => {
-//     enterButton.style.color = "olive"
-// })
-
-// document
-// .getElementById("enterButton")
-// .addEventListener("mouseup", () => {
-//     enterButton.style.color = "green"
-// })
-
 var form = document.inputForm
 var list = document.getElementById("list")
 
@@ -102,13 +14,13 @@ form.addEventListener("submit", function(e){
     list.append(newItem)
   
     //create buttons
-  
+
     //delete
     var removeButton = document.createElement("button")
     removeButton.textContent = "X"
     removeButton.classList.add("remove");
   
-    //mark
+    //edit
     var editButton = document.createElement("button")
     editButton.classList.add("edit")
     var editInput = document.createElement("input")
@@ -139,7 +51,7 @@ form.addEventListener("submit", function(e){
     removeButton.addEventListener("click", deleteItem)
     function deleteItem(event){
         if(event.target.classList.contains("remove")){
-            event.target.closest("li").remove();
+            event.target.closest("li").remove()
         }
     }
 
@@ -160,5 +72,5 @@ form.addEventListener("submit", function(e){
         inputButton.remove()
         editInput.remove()
     })
-        
+
 })
